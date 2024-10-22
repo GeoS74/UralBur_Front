cc8b5febd2a04dc0c8a6a59dd106e5b66b595693
Aboltys754 <warwar754@mail.ru>
2024-10-22 22:12:31 +0500

    add path img

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,54 +1,52 @@
-(() => {
-    const root = ReactDOM.createRoot(document.getElementById("header"));
+const root = ReactDOM.createRoot(document.getElementById("header"));
 
 
-    function Header() {
-      return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand " href="index.html"><img src="images/logo.svg" alt="Logo" width="60px" height="60px"/></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container-fluid">
+      <a className="navbar-brand " href="index.html"><img src="images/logo.svg" alt="Logo" width="100px" height="100px"/></a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-          <div class="collapse navbar-collapse" id="navbarsExample05">
-            <ul class="navbar-nav pl-md-5 ml-auto">
-              <li class="nav-item">
-                <a class="nav-link active" href="index.html">Главная</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.html">О компании</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="projects.html">Продукция</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="services.html" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Сервис</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown04">
-                  <a class="dropdown-item" href="services.html">Запчасти</a>
-                  <a class="dropdown-item" href="services.html">Индивидуальные решения</a>
-                  <a class="dropdown-item" href="services.html">Обслуживание</a>
-                </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="blog.html">Блог</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact.html">Контакты</a>
-              </li>
-            </ul>
-
-            <div class="navbar-nav ml-auto">
-              <form method="post" class="search-form">
-                <span class="icon ion ion-search"></span>
-                <input type="text" class="form-control" placeholder="Поиск..."></input>
-              </form>
+      <div className="collapse navbar-collapse" id="navbarsExample05">
+        <ul className="navbar-nav pl-md-5 ml-auto">
+          <li className="nav-item">
+            <a className="nav-link active" href="index.html">Главная</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="about.html">О компании</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="projects.html">Продукция</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="services.html" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Сервис</a>
+            <div className="dropdown-menu" aria-labelledby="dropdown04">
+              <a className="dropdown-item" href="services.html">Запчасти</a>
+              <a className="dropdown-item" href="services.html">Индивидуальные решения</a>
+              <a className="dropdown-item" href="services.html">Обслуживание</a>
             </div>
-            
-          </div>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="blog.html">Блог</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="contact.html">Контакты</a>
+          </li>
+        </ul>
+
+        <div className="navbar-nav ml-auto">
+          <form method="post" className="search-form">
+            <span className="icon ion ion-search"></span>
+            <input type="text" className="form-control" placeholder="Поиск..."></input>
+          </form>
         </div>
-      </nav>
-      )
-    }
-    root.render(/*#__PURE__*/React.createElement(Header, null));
-  })();
+        
+      </div>
+    </div>
+  </nav>
+  )
+}
+root.render(/*#__PURE__*/React.createElement(Header, null));
--- src/slider/slider.jsx
+++ src/slider/slider.jsx
@@ -1,33 +1,87 @@
-(() => {
-    const root = ReactDOM.createRoot(document.getElementById("slider"));
+const root = ReactDOM.createRoot(document.getElementById("slider"));
+const useState = React.useState;
+const useEffect = React.useEffect;
 
-    function slider() {
-        fetchAllImgSlider();
-        return <div><p>привет</p></div>       
-    }
-    root.render(/*#__PURE__*/React.createElement(slider, null));
-  })();
+function Slides(){
+  const [slides, setSlides] = useState(Array)
+  useEffect(() => {
+    fetch('http://localhost:8080/api/mcontent/slider/public/search')
+    .then((res) => res.json())
+    .then((data )=> setSlides(data))
+  }, [])
+
+  console.log(slides)
+  return (<>
+      {slides.map(slide => 
+          <div key={slide.id} className="slider-item" style={{backgroundImage: `url(http://localhost:8080/api/mcontent/static/images/slider/${slide.image.fileName})`}}>
+            <div className="container">
+              <div className="row slider-text align-items-center justify-content-center">
+                <div className="col-lg-7 text-center col-sm-12 element-animate">
+                  <div className="btn-play-wrap mx-auto"><p className="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" className="btn-play"><span className="ion ion-ios-play"></span></a></p></div>
+                  <h1 className="mb-4"><span>We Are Industrial Company</span></h1>
+                  <p className="mb-5 w-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias iste ipsa excepturi nostrum sequi molestias?</p>
+                </div>
+              </div>
+            </div>
+          </div>
+      )}
+  </>)
+};
 
 
-  async function fetchAllImgSlider() {
-    try {
-        const resp = await fetch('http://localhost:8080/api/mcontent/slider/public/search');
-        if (resp.ok) {
-            const json = await resp.json();
-            console.log(json)}
-    } catch (err) {
-        console.log(err)
-    }
-  }
 
-//   <div class="slider-item" style="background-image: url('images/industrial_hero_1.jpg');">
-//         <div class="container">
-//           <div class="row slider-text align-items-center justify-content-center">
-//             <div class="col-lg-7 text-center col-sm-12 element-animate">
-//               <div class="btn-play-wrap mx-auto"><p class="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" class="btn-play"><span class="ion ion-ios-play"></span></a></p></div>
-//               <h1 class="mb-4"><span>We Are Industrial Company</span></h1>
-//               <p class="mb-5 w-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias iste ipsa excepturi nostrum sequi molestias?</p>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
+
+
+
+
+root.render(/*#__PURE__*/React.createElement(Slides, null));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // fetch('http://localhost:8080/api/mcontent/slider/public/search')
+  // .then(async (res) => {
+  //   if (res.ok) {
+  //     const result = await res.json()
+  //     console.log(result)
+  //     return (<>
+  //       {result.map(slide => {
+  //         <p>{slide.id}</p>
+  //       })}
+  //     </>)
+
+
+
+      
+      // return (<>
+      //   {
+      //     result.map(slide => {
+            // <div key={slide.key} className="slider-item" style={`background-image: url("http://localhost:8080//api/mcontent/static/images/slider/${slide.image.fileName}");`}>
+            //   <div className="container">
+            //     <div className="row slider-text align-items-center justify-content-center">
+            //       <div className="col-lg-7 text-center col-sm-12 element-animate">
+            //         <div className="btn-play-wrap mx-auto"><p className="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" className="btn-play"><span className="ion ion-ios-play"></span></a></p></div>
+            //         <h1 className="mb-4"><span>We Are Industrial Company</span></h1>
+            //         <p className="mb-5 w-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias iste ipsa excepturi nostrum sequi molestias?</p>
+            //       </div>
+            //     </div>
+            //   </div>
+            // </div>
+      //     })
+      //   }
+      // </>)
+  //   }
+  // })
+// };
+
+// root.render(/*#__PURE__*/React.createElement(Slides, null));
