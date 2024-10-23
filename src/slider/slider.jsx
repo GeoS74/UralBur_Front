8d88762c51a909a7bd4945d58ae9cb6aad0c6fb7
Aboltys754 <warwar754@mail.ru>
2024-10-23 11:08:06 +0500

    add title and message

--- src/slider/slider.jsx
+++ src/slider/slider.jsx
@@ -9,9 +9,7 @@ function Slides(){
     .then((res) => res.json())
     .then((data )=> setSlides(data))
   }, [])
-
-
-  console.log(slides)
+  
   const startSlide = <>
                         <div className="slider-item" style={{backgroundImage: `url(images/start_fon.jpg)`}}>
                           <div className="container">
@@ -31,9 +29,8 @@ function Slides(){
                             <div className="container">
                               <div className="row slider-text align-items-center justify-content-center">
                                 <div className="col-lg-7 text-center col-sm-12 element-animate">
-                                  <div className="btn-play-wrap mx-auto"><p className="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" className="btn-play"><span className="ion ion-ios-play"></span></a></p></div>
-                                  <h1 className="mb-4"><span>We Are Industrial Company</span></h1>
-                                  <p className="mb-5 w-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias iste ipsa excepturi nostrum sequi molestias?</p>
+                                  <h1 className="mb-4"><span>{slide.title}</span></h1>
+                                  <p className="mb-5 w-75">{slide.message}</p>
                                 </div>
                               </div>
                             </div>
