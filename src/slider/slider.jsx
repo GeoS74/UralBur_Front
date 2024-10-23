dd7c4828b3dd910db3d23d5a64eb1b11319ae03f
Aboltys754 <warwar754@mail.ru>
2024-10-23 10:52:06 +0500

    add start slide

--- src/slider/slider.jsx
+++ src/slider/slider.jsx
@@ -10,78 +10,40 @@ function Slides(){
     .then((data )=> setSlides(data))
   }, [])
 
+
   console.log(slides)
+  const startSlide = <>
+                        <div className="slider-item" style={{backgroundImage: `url(images/start_fon.jpg)`}}>
+                          <div className="container">
+                            <div className="row slider-text align-items-center justify-content-center">
+                              <div className="col-lg-7 text-center col-sm-12 element-animate">
+                                <h1 className="mb-4"><span>Мы первые во всем</span></h1>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                     </>
+
+
+  const listSlide = <>
+                      {slides.map(slide => 
+                          <div key={slide.id} className="slider-item" style={{backgroundImage: `url(http://localhost:8080/api/mcontent/static/images/slider/${slide.image.fileName})`}}>
+                            <div className="container">
+                              <div className="row slider-text align-items-center justify-content-center">
+                                <div className="col-lg-7 text-center col-sm-12 element-animate">
+                                  <div className="btn-play-wrap mx-auto"><p className="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" className="btn-play"><span className="ion ion-ios-play"></span></a></p></div>
+                                  <h1 className="mb-4"><span>We Are Industrial Company</span></h1>
+                                  <p className="mb-5 w-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias iste ipsa excepturi nostrum sequi molestias?</p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                      )}
+                    </>
   return (<>
-      {slides.map(slide => 
-          <div key={slide.id} className="slider-item" style={{backgroundImage: `url(http://localhost:8080/api/mcontent/static/images/slider/${slide.image.fileName})`}}>
-            <div className="container">
-              <div className="row slider-text align-items-center justify-content-center">
-                <div className="col-lg-7 text-center col-sm-12 element-animate">
-                  <div className="btn-play-wrap mx-auto"><p className="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" className="btn-play"><span className="ion ion-ios-play"></span></a></p></div>
-                  <h1 className="mb-4"><span>We Are Industrial Company</span></h1>
-                  <p className="mb-5 w-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias iste ipsa excepturi nostrum sequi molestias?</p>
-                </div>
-              </div>
-            </div>
-          </div>
-      )}
+      {slides.length !== 0 ? listSlide : startSlide}
   </>)
 };
 
 
-
-
-
-
-
 root.render(/*#__PURE__*/React.createElement(Slides, null));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // fetch('http://localhost:8080/api/mcontent/slider/public/search')
-  // .then(async (res) => {
-  //   if (res.ok) {
-  //     const result = await res.json()
-  //     console.log(result)
-  //     return (<>
-  //       {result.map(slide => {
-  //         <p>{slide.id}</p>
-  //       })}
-  //     </>)
-
-
-
-      
-      // return (<>
-      //   {
-      //     result.map(slide => {
-            // <div key={slide.key} className="slider-item" style={`background-image: url("http://localhost:8080//api/mcontent/static/images/slider/${slide.image.fileName}");`}>
-            //   <div className="container">
-            //     <div className="row slider-text align-items-center justify-content-center">
-            //       <div className="col-lg-7 text-center col-sm-12 element-animate">
-            //         <div className="btn-play-wrap mx-auto"><p className="mb-4"><a href="https://vimeo.com/59256790" data-fancybox data-ratio="2" className="btn-play"><span className="ion ion-ios-play"></span></a></p></div>
-            //         <h1 className="mb-4"><span>We Are Industrial Company</span></h1>
-            //         <p className="mb-5 w-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias iste ipsa excepturi nostrum sequi molestias?</p>
-            //       </div>
-            //     </div>
-            //   </div>
-            // </div>
-      //     })
-      //   }
-      // </>)
-  //   }
-  // })
-// };
-
-// root.render(/*#__PURE__*/React.createElement(Slides, null));
