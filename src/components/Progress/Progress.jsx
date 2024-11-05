7da487260c6bbcdb50039633162423c2d3297906
Aboltys754 <warwar754@mail.ru>
2024-11-05 10:54:11 +0500

    list restriction

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -1,13 +1,17 @@
 import serviceHost from "../libs/service.host.js";
 
 function Progress({ info }) {
-    return info.map((e) => <div key={e.id} className="feature-1 d-md-flex">
-                              <div className="align-self-center">
-                                <span className={`ion ${e.cssClass} display-4 text-primary`}></span>
-                                <h3>{`${e.title}`}</h3>
-                                <p>{`${e.message}`}</p>
-                              </div>
-                           </div>)
+  if (info.length > 2) {
+    shuffle(info)
+    info = info.slice(0, 2)
+  }
+  return info.map((e) => <div key={e.id} className="feature-1 d-md-flex">
+                            <div className="align-self-center">
+                              <span className={`ion ${e.cssClass} display-4 text-primary`}></span>
+                              <h3>{`${e.title}`}</h3>
+                              <p>{`${e.message}`}</p>
+                            </div>
+                          </div>)
 }
 
 
@@ -20,3 +24,10 @@ fetch(`${serviceHost("mcontent")}/api/mcontent/progress/public/search`)
     const root = ReactDOM.createRoot(document.getElementById("progress"));
     root.render(<Progress info={res} />);
   })
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
--- src/components/Solution/Solution.jsx
+++ src/components/Solution/Solution.jsx
@@ -1,13 +1,17 @@
 import serviceHost from "../libs/service.host.js";
 
 function Solution({ info }) {
-    return info.map((e) => <div key={e.id} className="feature-1 d-md-flex">
-                              <div className="align-self-center">
-                                <span className={`ion ${e.cssClass} display-4 text-primary`}></span>
-                                <h3>{`${e.title}`}</h3>
-                                <p>{`${e.message}`}</p>
-                              </div>
-                           </div>)
+  if (info.length > 2) {
+    shuffle(info)
+    info = info.slice(0, 2)
+  }
+  return info.map((e) => <div key={e.id} className="feature-1 d-md-flex">
+                            <div className="align-self-center">
+                              <span className={`ion ${e.cssClass} display-4 text-primary`}></span>
+                              <h3>{`${e.title}`}</h3>
+                              <p>{`${e.message}`}</p>
+                            </div>
+                          </div>)
 }
 
 
@@ -20,3 +24,10 @@ fetch(`${serviceHost("mcontent")}/api/mcontent/solution/public/search`)
     const root = ReactDOM.createRoot(document.getElementById("solution"));
     root.render(<Solution info={res} />);
   })
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
