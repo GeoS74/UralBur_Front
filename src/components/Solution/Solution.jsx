565d687f36ea9da4b597e0697f4ec274c489b398
Aboltys754 <warwar754@mail.ru>
2024-11-06 13:23:33 +0500

    refactoring progress and solution

--- src/components/Solution/Solution.jsx
+++ src/components/Solution/Solution.jsx
@@ -1,19 +1,35 @@
 import serviceHost from "../libs/service.host.js";
 
+const defaulId = ['sdhbfpWJHPFwh732468f8w6v0', 'sdhbfpWJHPFwh732468f8w6v1', 'sdhbfpWJHPFwh732468f8w6v2',]
+const defaulClass = ['ion-android-alarm-clock', 'ion-android-calendar', 'ion-android-car']
+const defaulTitle = ['Время важно для нас', 'Все в срок', 'Наше решение для вас']
+const defaulMessage = ['Вы еще думаете, а мы уже cделали', 
+                      'Чательно выполняем все в срок', 
+                      'Каждая машина продумывается для вас']
+
 function Solution({ info }) {
-  if (info.length > 2) {
+
+  if (info.length > 3) {
     shuffle(info)
     info = info.slice(0, 2)
+  } else if (info.length < 3) {
+    let i = info.length
+    while(i < 4) {
+      info.push({id: defaulId[i], cssClass: defaulClass[i], title: defaulTitle[i], message: defaulMessage[i]})
+      i++
+    }    
   }
-  return info.map((e) => <div key={e.id} className="feature-1 d-md-flex">
-                            <div className="align-self-center">
-                              <span className={`ion ${e.cssClass} display-4 text-primary`}></span>
-                              <h3>{`${e.title}`}</h3>
-                              <p>{`${e.message}`}</p>
-                            </div>
+
+  return info.map((e) => <div key={e.id} className="col-md-6 col-lg-4 element-animate ">
+                            <div className="media block-6 d-block text-center">
+                              <div className="icon mb-3"><span className="ion-flash text-primary"></span></div>
+                              <div className="media-body">
+                                <h3 className="heading">{e.title}</h3>
+                                <p>{e.message}</p>
+                              </div>
+                            </div> 
                           </div>)
 }
-
 
 fetch(`${serviceHost("mcontent")}/api/mcontent/solution/public/search`)
   .then(async response => {
