8167bce84cd0c2af1a0454c864d55111a9cd647c
geos <gsirotkin@yandex.ru>
2024-12-03 17:53:43 +0500

    refactoring footer

--- src/components/MainCatalog/MainCatalog.jsx
+++ src/components/MainCatalog/MainCatalog.jsx
@@ -1,47 +1,63 @@
 function MainCatalog() {
   return <>
-    <div className="col-md-4 element-animate">
-      <a href="project-single.html" className="link-thumbnail">
-        <h3>Супер буры</h3>
-        <span className="ion-plus icon"></span>
-        <img src="images/industrial_img_1.jpg" alt="Free template by Free-Template.co" className="img-fluid"/>
-      </a>
+    <div class="container">
+      <div class="row justify-content-center mb-5 element-animate">
+        <div class="col-md-8 text-center">
+          <h2 class=" heading mb-4">Продукция</h2>
+          <p class="mb-5 lead">Мы поставляем качественную и востребованную всеми компаниями продукцию</p>
+        </div>
+      </div>
     </div>
-    <div className="col-md-4 element-animate">
-      <a href="project-single.html" className="link-thumbnail">
-        <h3>Бур 3000</h3>
-        <span className="ion-plus icon"></span>
-        <img src="images/industrial_img_2.jpg" alt="Free template by Free-Template.co" className="img-fluid"/>
-      </a>
+
+    <div class="container-fluid">
+      <div class="row no-gutters">
+        <div className="col-md-4 element-animate">
+          <a href="project-single.html" className="link-thumbnail">
+            <h3>Супер буры</h3>
+            <span className="ion-plus icon"></span>
+            <img src="images/industrial_img_1.jpg" alt="Free template by Free-Template.co" className="img-fluid" />
+          </a>
+        </div>
+        <div className="col-md-4 element-animate">
+          <a href="project-single.html" className="link-thumbnail">
+            <h3>Бур 3000</h3>
+            <span className="ion-plus icon"></span>
+            <img src="images/industrial_img_2.jpg" alt="Free template by Free-Template.co" className="img-fluid" />
+          </a>
+        </div>
+        <div className="col-md-4 element-animate">
+          <a href="project-single.html" className="link-thumbnail">
+            <h3>Мега бурилка</h3>
+            <span className="ion-plus icon"></span>
+            <img src="images/industrial_img_3.jpg" alt="Free template by Free-Template.co" className="img-fluid" />
+          </a>
+        </div>
+        <div className="col-md-4 element-animate">
+          <a href="project-single.html" className="link-thumbnail">
+            <h3>Пробивной бур</h3>
+            <span className="ion-plus icon"></span>
+            <img src="images/industrial_img_4.jpg" alt="Free template by Free-Template.co" className="img-fluid" />
+          </a>
+        </div>
+        <div className="col-md-4 element-animate">
+          <a href="project-single.html" className="link-thumbnail">
+            <h3>Бур как бур</h3>
+            <span className="ion-plus icon"></span>
+            <img src="images/industrial_img_5.jpg" alt="Free template by Free-Template.co" className="img-fluid" />
+          </a>
+        </div>
+        <div className="col-md-4 element-animate">
+          <a href="project-single.html" className="link-thumbnail">
+            <h3>Бурильная камера</h3>
+            <span className="ion-plus icon"></span>
+            <img src="images/industrial_img_6.jpg" alt="Free template by Free-Template.co" className="img-fluid" />
+          </a>
+        </div>
+      </div>
     </div>
-    <div className="col-md-4 element-animate">
-      <a href="project-single.html" className="link-thumbnail">
-        <h3>Мега бурилка</h3>
-        <span className="ion-plus icon"></span>
-        <img src="images/industrial_img_3.jpg" alt="Free template by Free-Template.co" className="img-fluid"/>
-      </a>
-    </div>
-    <div className="col-md-4 element-animate">
-      <a href="project-single.html" className="link-thumbnail">
-        <h3>Пробивной бур</h3>
-        <span className="ion-plus icon"></span>
-        <img src="images/industrial_img_4.jpg" alt="Free template by Free-Template.co" className="img-fluid"/>
-      </a>
-    </div>
-    <div className="col-md-4 element-animate">
-      <a href="project-single.html" className="link-thumbnail">
-        <h3>Бур как бур</h3>
-        <span className="ion-plus icon"></span>
-        <img src="images/industrial_img_5.jpg" alt="Free template by Free-Template.co" className="img-fluid"/>
-      </a>
-    </div>
-    <div className="col-md-4 element-animate">
-      <a href="project-single.html" className="link-thumbnail">
-        <h3>Бурильная камера</h3>
-        <span className="ion-plus icon"></span>
-        <img src="images/industrial_img_6.jpg" alt="Free template by Free-Template.co" className="img-fluid"/>
-      </a>
-    </div>
+
+
+
   </>
 }
 
