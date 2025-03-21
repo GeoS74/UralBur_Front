c52fbf1969314fdf12f56311a469e9843f9b4cc7
GeoS74 <gsirotkin@yandex.ru>
2025-03-21 20:01:25 +0300

    pdf started

--- src/components/Catalog/Position/Position.jsx
+++ src/components/Catalog/Position/Position.jsx
@@ -3,11 +3,51 @@ import connector from "../../libs/connector.js";
 
 connector.add("Position");
 
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'js/pdf/pdf.worker.js'
+    // '../../node_modules/pdfjs-dist/build/pdf.worker.mjs';
+
 function Position({ position }) {
   React.useEffect(() => connector.del("Position"));
+  // console.log(position)
+
+  const loadingTask = pdfjsLib.getDocument(`${serviceHost("mcontent")}/api/mcontent/static/catalog/position/pdf/${position.files.pdf.fileName}`);
+  
+  loadingTask.promise.then(pdf => {
+    console.log('pdf');
+    console.log(pdf);
+    return pdf.getPage(1);
+  })
+  .then(page => {
+    console.log('page');
+    // return pdf.getPage(1);
+
+    const scale = 0.5;
+    const viewport = page.getViewport({ scale });
+    const outputScale = window.devicePixelRatio || 1;
+    const canvas = document.getElementById("the-canvas");
+    const context = canvas.getContext("2d");
+
+    canvas.width = Math.floor(viewport.width * outputScale);
+    canvas.height = Math.floor(viewport.height * outputScale);
+    canvas.style.width = Math.floor(viewport.width) + "px";
+    canvas.style.height = Math.floor(viewport.height) + "px";
+
+    const transform = outputScale !== 1
+      ? [outputScale, 0, 0, outputScale, 0, 0]
+      : null;
+
+      const renderContext = {
+        canvasContext: context,
+        transform,
+        viewport,
+      };
+      page.render(renderContext);
+  });
+
+  return <div className="container element-animate">
+     
 
 
-  return <div className="container">
     <div className="row justify-content-center"  style={{marginTop: -50}}>
       <div className="col-md-8 mb-5">
         <h2>&ldquo; {position.title} &rdquo;</h2>
@@ -35,6 +75,12 @@ function Position({ position }) {
         {/* <p><a href="#" className="btn btn-primary py-3 px-3">Visit Website</a></p> */}
       </div>
     </div>
+
+
+    <div className="row justify-content-center element-animate">
+    <canvas id="the-canvas"></canvas>
+    </div>
+
   </div>
 }
 
