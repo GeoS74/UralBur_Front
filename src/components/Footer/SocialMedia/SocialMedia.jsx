392d7718230eb96d73a9e79466307a529145b104
GeoS74 <gsirotkin@yandex.ru>
2025-06-30 20:14:17 +0300

    added md-conv, link to telegram and fixed nginx config

--- src/components/Footer/SocialMedia/SocialMedia.jsx
+++ src/components/Footer/SocialMedia/SocialMedia.jsx
@@ -3,7 +3,7 @@ export default function SocialMedia() {
     <h3>Мы в соц. сетях</h3>
     <p className="mb-5">Мы всегда готовы развиваться и идти в ногу со временем. Выбирая нас вы выбираете качество.</p>
     <ul className="list-unstyled footer-link d-flex footer-social">
-      <li><a  href="#" className="p-2"><span className="fa fa-telegram"></span></a></li>
+      <li><a  href="https://t.me/urb2A" className="p-2" target="blank"><span className="fa fa-telegram"></span></a></li>
     </ul>
   </div>
 }
